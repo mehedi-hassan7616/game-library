2f5d106bb96673838673249862c2e28a83ba9b52
Mehedi Hassan <mhkmehedihassan@gmail.com>
2025-10-25 15:31:23 +0600

    epdate file

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,77 +1,105 @@
-// import React, { use, useState } from 'react';
-// import Navbar from '../component/Navbar';
-// import { Link } from 'react-router';
-// import { AuthContext } from '../provider/Authprovider';
-// // import { useContext } from 'react';
-// const Login = () => {
-//     const { signIn } = use(AuthContext);
-//     const [error, setError] = useState('');
-//     const handleLogin = e => {
-//         e.preventDefault();
-//         const email = e.target.email.value;
-//         const password = e.target.password.value;
-//         // console.log(email, password)
-//         setError('');
-//         signIn(email, password)
-//             .then((result) => {
-//                 const user = result.user;
-//                 console.log(user)
-//                 if (!user.emailVerified) {
-//                     alert("Verification your email")
-//                 }
-//             })
-//             .catch((error) => {
-//                 const errorMessage = error.message;
-//                 console.log(errorMessage)
-//                 // setError(errorMessage);
-//             });
-//     }
-//     const handleForgatePass = () => {
-//         console.log('forget password')
-//         // const email = e
-//     }
-//     return (
-//        <>
-//        <Navbar></Navbar>
-//             <div className='pt-40'>
-//                 <div className='container mx-auto space-y-4 py-4'>
-//                     <title> Login</title>
-//                     <div className="flex justify-center">
-//                         <form onSubmit={handleLogin} action="">
-//                             <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-//                                 <legend className="fieldset-legend pt-10 text-2xl font-bold">Login</legend>
-//                                 <label className="label">Email</label>
-//                                 <input
-//                                     type="email"
-//                                     name="email"
-//                                     className="input"
-//                                     placeholder="Email"
-//                                 />
-//                                 <label className="label">Password</label>
-//                                 <input
-//                                     type="password"
-//                                     name="password"
-//                                     className="input"
-//                                     placeholder="Password"
-//                                 />
-//                                 <div onClick={handleForgatePass}>
-//                                     {" "}
-//                                     <a className="text-blue-500">forget password</a>
-//                                 </div>
-//                                 <button className="btn btn-neutral mt-4">Login</button>
-//                             </fieldset>
-//                             {error && <p className="text-red-500">{error}</p>}
-//                             <p>
-//                                 new to game-library account ? please
-//                                 <Link className="text-blue-500 underline" to="/register">
-//                                     register
-//                                 </Link>
-//                             </p>
-//                         </form>
-//                     </div>
-//                 </div>
-//             </div>
-//        </>
-//     );
-// };
-// export default Login;
+import React, { use, useRef, useState } from 'react';
+import Navbar from '../component/Navbar';
+import { Link } from 'react-router';
+import { AuthContext } from '../provider/Authprovider';
+import { sendPasswordResetEmail } from 'firebase/auth';
+// import { useContext } from 'react';
+const Login = () => {
+    const emailref = useRef("")
+    const { signIn, logInWithGoogle, profileUpdate } = use(AuthContext);
+    const [error, setError] = useState('');
+    const handleLogin = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        // console.log(email, password)
+        setError('');
+        signIn(email, password)
+            .then((result) => {
+                const user = result.user;
+                console.log(user)
+                if (!user.emailVerified) {
+                    alert("Verification your email")
+                }
+            })
+            .catch((error) => {
+                const errorMessage = error.message;
+                console.log(errorMessage)
+                // setError(errorMessage);
+            });
+    }
+    const handleGoogle = (e) => {
+        e.preventDefault();
+        logInWithGoogle()
+            .then((result) => {
+                const user = result.user;
+                console.log(user)
+                if (!user.emailVerified) {
+                    alert("Verification your email")
+                }
+            })
+            .catch((error) => {
+                const errorMessage = error.message;
+                console.log(errorMessage)
+                // setError(errorMessage);
+            });
+    };
+    const handleForgatePass = () => {
+        const email = emailref.current.value;
+        console.log('forget password ', email);
+        sendPasswordResetEmail(auth, email)
+            .then(() => {
+                alert('please check your email')
+            })
+            .catch((error) => {
+                // console.log(error);
+            });
+    }
+    return (
+        <>
+            <div className='pt-40'>
+                <div className='container mx-auto space-y-4 py-4'>
+                    <title> Login</title>
+                    <div className="flex justify-center">
+                        <form onSubmit={(e) => { handleLogin(e); handleGoogle(e); }} action="">
+                            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                                <legend className="fieldset-legend pt-10 text-2xl font-bold">Login</legend>
+                                <label className="label">Email</label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    className="input"
+                                    ref={emailref}
+                                    placeholder="Email"
+                                />
+                                <label className="label">Password</label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    className="input"
+                                    placeholder="Password"
+                                />
+                                <div onClick={handleForgatePass}>
+                                    {" "}
+                                    <a className="text-blue-500">forget password</a>
+                                </div>
+                                <div className='flex justify-between'>
+                                    <button className="btn btn-neutral mt-4">Email Login</button>
+                                    <button className="btn btn-neutral mt-4"> Google Login</button>
+                                </div>
+                            </fieldset>
+                            {error && <p className="text-red-500">{error}</p>}
+                            <p>
+                                new to game-library account ? please{" "}
+                                <Link className="text-blue-500 underline" to="/register">
+                                    register
+                                </Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+export default Login;
--- src/provider/Authprovider.jsx
+++ src/provider/Authprovider.jsx
@@ -1,20 +1,34 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile, GoogleAuthProvider, sendEmailVerification } from "firebase/auth";
 import app from '../firebase/firebase.Config';
 export const AuthContext = createContext()
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 const Authprovider = ({ children }) => {
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(null);
+    const [loader, setLoader] = useState(true);
     // console.log(user)
     const createUser = (email, password) => {
+        setLoader(true);
         return createUserWithEmailAndPassword(auth, email, password);
+        sendEmailVerification(auth.currentUser)
     }
     const signIn = (email, password) => {
+        setLoader(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
     const logOut = () => {
+        setLoader(true);
         return signOut(auth)
     }
+    const logInWithGoogle = (googleProvider) => {
+        setLoader(true);
+        return signInWithPopup(auth, googleProvider);
+    };
+    const profileUpdate = (profile) => {
+        setLoader(true);
+        return updateProfile(auth.currentUser, profile);
+    };
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser)
@@ -24,6 +38,10 @@ const Authprovider = ({ children }) => {
         }
     }, [])
     const authdata = {
+        loader,
+        setLoader,
+        logInWithGoogle,
+        profileUpdate,
         signIn,
         logOut,
         createUser,
